df32eab3d2d7d5cc276377e844fec6b1303cbd08
wangfengshuo <wangfengshuo2012@163.com>
2023-08-01 16:19:57 +0800

    proto试用demo，可正常运行，补充提交遗漏的工程文件

---
+++ prototest/src/main/java/com/example/prototest/PrototestApplication.java
@@ -0,0 +1,13 @@
+package com.example.prototest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototestApplication.class, args);
+	}
+
+}
---
+++ prototest/src/test/java/com/example/prototest/PrototestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prototest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
