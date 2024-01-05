46755a2500b9584f073f37d4d949282a7e989093
wangfengshuo <wangfengshuo2012@163.com>
2024-01-05 11:44:58 +0800

    test spi：SPI_caller

---
+++ SPI_caller/src/main/java/com/example/template/SpiTestDemo.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : NERV
+ * Module Name  : SpiTestDemo
+ * Date Created : 2024-01-04
+ * Creator      : w15021
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2024-01-04       w15021     xxx
+ * --------------------------------------------------------------------
+ */
+
+package com.example.template;
+
+/**
+ * @program: com.example.template
+ * @description: xxx
+ * @author: w15021
+ * @create: 2024-01-04
+ **/
+// 服务调用方需要的服务
+public interface SpiTestDemo {
+    void way1();
+    void way2();
+}
---
+++ SPI_caller/src/main/java/com/example/template/TemplateApplication.java
@@ -0,0 +1,14 @@
+package com.example.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// 参考：https://juejin.cn/post/6882758635781685261
+// 打jar包 https://m.tongfu.net/home/35/blog/513629.html#5.
+@SpringBootApplication
+public class TemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateApplication.class, args);
+	}
+
+}
